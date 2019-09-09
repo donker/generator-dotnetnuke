db0f60ebb7b0042636a95d011eb1080beb0ff317
donker <peter@bring2mind.net>
2019-09-09 17:09:44 +0200

    Cleaning up

--- generators/react/templates/webpack.config.js
+++ generators/react/templates/webpack.config.js
@@ -1,18 +1,11 @@
 var path = require("path"),
-  pkg = require("../../../package.json"),
   commonConfig = require("../../webpack.common.config");
-
-var isProduction =
-  process.argv.indexOf("-p") >= 0 || process.env.NODE_ENV === "production";
 
 var clientAppConfig = Object.assign({}, commonConfig, {
   context: path.join(__dirname, "."),
   entry: "./App.tsx",
   output: {
-    path: isProduction
-      ? path.resolve(__dirname, "../../../Server/<%= Name %>/js")
-      : pkg.dnn.pathsAndFiles.devSitePath +
-        "\\DesktopModules\\MVC\\FormaMed\\<%= ModuleName %>\\js",
+    path: path.resolve(__dirname, "../../../Server/<%= Name %>/js"),
     filename: "<%= Name.toLowerCase() %>.js"
   },
   resolve: {
